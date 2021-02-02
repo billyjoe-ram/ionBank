f765e84c69088af2d1e4585c8825f5322201d06d
Billy Joe Ramos <billyjoersantos@outlook.com>
2021-02-02 11:24:15 -0300

    Header e footer

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public options: any[] = [
+    { icon: "person-add-outline", text: "Indicar amigos"},
+    { icon: "phone-portrait-outline", text: "Recarga de celular"},
+    { icon: "wallet-outline", text: "Depositar"},
+    { icon: "options-outline", text: "Ajustar limite"},    
+    { icon: "card-outline", text: "Cartão virtual"},
+    { icon: "help-circle-outline", text: "Ajuda"}
+  ];
+
+  public slidesOptions: any = { slidesPerView: 3, freeMode: true }
 
   constructor() {}
 
